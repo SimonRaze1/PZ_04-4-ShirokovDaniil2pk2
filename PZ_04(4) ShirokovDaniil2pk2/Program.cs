60d385529674b343ce1a5e7602a46a91918feb87
User <User@DESKTOP-AMGP8UK>
2023-09-23 10:41:20 +0500

    Добавьте файлы проекта.

---
+++ PZ_04(4) ShirokovDaniil2pk2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PZ_04_4__ShirokovDaniil2pk2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = -900; i <= 500; i++)
+            {
+                if (i % 16 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
